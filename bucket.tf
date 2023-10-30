48092d1fb360c44d058e20770096596781eeb1c9
mviamonte <66328257+mviamonte@users.noreply.github.com>
2023-10-30 16:55:43 +0100

    Removing storage class

--- bucket.tf
+++ bucket.tf
@@ -1,7 +1,6 @@
 resource "aws_s3_bucket" "bucket_from_gha" {
   bucket = "mv-bucket-gha-test"
-  storage_class = ONEZONE_IA
-
+  
   tags = {
     Name        = "MV-Bucket"
     Environment = "Prod"
