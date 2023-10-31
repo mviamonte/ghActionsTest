446b0050d93f48b64b71004162079fdc0554376c
mviamonte <66328257+mviamonte@users.noreply.github.com>
2023-10-31 11:36:17 +0100

    Adding env variables

--- main.tf
+++ main.tf
@@ -19,6 +19,7 @@ provider "aws" {
   region = "eu-central-1"
   assume_role_with_web_identity {
       role_arn = "arn:aws:iam::076080011360:role/GithubActionsRole"
+      web_identity_token = env("AWS_SESSION_TOKEN")
       }
   }
 
